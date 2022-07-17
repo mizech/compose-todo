bfb80d4640a1b20c081168831682636e577fd575
michael.zech <michael.zech@hotmail.de>
2022-07-17 09:27:15 +0200

    Added label and placeholder to the text-field.

--- app/src/main/java/com/mizech/compose_todo/MainActivity.kt
+++ app/src/main/java/com/mizech/compose_todo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mizech.compose_todo.ui.theme.ComposetodoTheme
@@ -51,6 +53,10 @@ fun MainView() {
         horizontalAlignment = Alignment.CenterHorizontally) {
         TextField(value = currentText, onValueChange = {
             currentText = it
+        }, label = {
+            Text(text = "Add new To-Do")
+        }, placeholder = {
+            Text(text = "What has to be done?")
         }, modifier = Modifier.padding(top = 20.dp, bottom = 10.dp))
         Button(onClick = {
             todos.add(Todo(text = currentText))
