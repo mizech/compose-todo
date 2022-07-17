b2bd2226de3d7054780f350fe3cab27aa4c72a3c
michael.zech <michael.zech@hotmail.de>
2022-07-17 09:19:42 +0200

    Added mutableState

--- app/src/main/java/com/mizech/compose_todo/MainActivity.kt
+++ app/src/main/java/com/mizech/compose_todo/MainActivity.kt
@@ -1,30 +1,27 @@
 package com.mizech.compose_todo
 
 import Todo
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mizech.compose_todo.ui.theme.ComposetodoTheme
 
 class MainActivity : ComponentActivity() {
-    val todos = arrayListOf<Todo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        todos.add(Todo("Alpha 01"))
-        todos.add(Todo("Alpha 02"))
-        todos.add(Todo("Alpha 03"))
-        todos.add(Todo("Alpha 04"))
-        todos.add(Todo("Alpha 05"))
 
         setContent {
             ComposetodoTheme {
@@ -32,19 +29,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MainView(todos = todos)
+                    MainView()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun MainView(todos: ArrayList<Todo>) {
-    Row(modifier = Modifier.fillMaxSize()) {
+fun MainView() {
+    var todos = remember {
+        mutableStateListOf<Todo>()
+    }
+
+    var currentText by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        TextField(value = currentText, onValueChange = {
+            currentText = it
+        }, modifier = Modifier.padding(top = 20.dp, bottom = 10.dp))
+        Button(onClick = {
+            todos.add(Todo(text = currentText))
+            currentText = ""
+        }) {
+            Text("Insert new To-Do")
+        }
         LazyColumn {
             items(todos.count()) { index ->
-                Text(todos.get(index).text)
+                Text(text = todos.get(index).text,
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp))
             }
         }
     }
@@ -54,6 +71,6 @@ fun MainView(todos: ArrayList<Todo>) {
 @Composable
 fun DefaultPreview() {
     ComposetodoTheme {
-        MainView(todos = arrayListOf(Todo("", false)))
+        MainView()
     }
 }
